6314e838710820dd10a688180ac86e9ab746304e
Igor Mogilin <i@igorm.org>
2015-10-26 22:09:34 +0200

    added element.on 'dragstart'

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -400,3 +400,22 @@ angular.module('dndLists', [])
     return function(scope, element, attr) {
       // Set as draggable so that we can cancel the events explicitly
       element.attr("draggable", "true");
+
+      /**
+       * Since the element is draggable, the browser's default operation is to drag it on dragstart.
+       * We will prevent that and also stop the event from bubbling up.
+       */
+      element.on('dragstart', function(event) {
+        event = event.originalEvent || event;
+
+        // If a child element already reacted to dragstart and set a dataTransfer object, we will
+        // allow that. For example, this is the case for user selections inside of input elements.
+        if (!(event.dataTransfer.types && event.dataTransfer.types.length)) {
+          event.preventDefault();
+        }
+        event.stopPropagation();
+      });
+
+    };
+  })
+
